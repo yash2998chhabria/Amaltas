95f0ec1831b175dc6d23d57ee2fbfe7d0286fdd6
Yash Chhabria <yash2998chhabria@gmail.com>
2020-06-29 18:33:26 +0530

    categories added again

--- category/views.py
+++ category/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from stalls.models import stall_frame,stall_products
-
+from home.models import MakeVisible
 # Create your views here.
 
 def categorize(request,name):
+	visibility = MakeVisible.objects.all()[0].start_exhibition
 	products = stall_products.objects.all()
 	stalls = stall_frame.objects.all()
 	prod_list = []
@@ -15,23 +16,31 @@ def categorize(request,name):
 				if str(product.stall_name)==str(stall.name):
 					prod={
 					'product': product,
-					'stall': stall
+					'stall': stall,
+					'visibility':visibility
 					} 				
 					return render(request,"product_page.html",prod)
 	if	len(prod_list) !=0:				
 		prod_info ={
 			'products':prod_list,
-			'category':name
+			'category':name,
+			'visibility':visibility
 		}		
 		return render(request,'specific_category.html',prod_info)	
 
 def display_all(request):
+	visibility = MakeVisible.objects.all()[0].start_exhibition
+	print(visibility)
 	products = stall_products.objects.all()
 	present_categories = []
 	check_categories = []
 	for product in products:
 		if (product.category not in check_categories):
 			check_categories.append(product.category)
-			present_categories.append(product)		
-	return render(request,"all_categories.html",{'present_categories':present_categories})
+			present_categories.append(product)
+	context = {
+				'present_categories':present_categories,
+				'visibility':visibility
+				}				
+	return render(request,"all_categories.html",context)
 							
