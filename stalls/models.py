713792bb9454d62f197848ba9e54a1bec1a6a65b
Yash Chhabria <yash2998chhabria@gmail.com>
2020-07-22 22:31:14 +0530

    Update models.py

--- stalls/models.py
+++ stalls/models.py
@@ -22,6 +22,7 @@ def compress(image):
     im = Image.open(image)
     im_io = BytesIO() 
     im.save(im_io, 'JPEG', quality=60) 
+    im_io.seek(0)
     new_image = File(im_io, name=image.name)
     return new_image			
 
