632a8dd96e3b4db8e35cff890dd7ef32bd24c9b7
Yash Chhabria <yash2998chhabria@gmail.com>
2020-08-21 13:58:07 +0530

    React dev setup
    react, babel, webpack setup

---
+++ static/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Main from './Components/MainComponent'
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+            <h1>This is JSX. LETS FUCK SHIT UP</h1>
+            <Main />
+            </div>
+        );
+    }    
+}
+
+export default App;
