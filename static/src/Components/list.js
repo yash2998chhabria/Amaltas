54b4fd7a3c0b1ca65f368404baf39c7fbd06a8d0
Yash Chhabria <yash2998chhabria@gmail.com>
2020-08-24 16:48:20 +0530

    for server

--- static/src/Components/list.js
+++ static/src/Components/list.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle, Button } from 'reactstrap';
-import { Link } from 'react-router-dom'
-import { Loading } from '../Components/LoadingComponent'
+import { Link } from 'react-router-dom';
+import { Loading } from '../Components/LoadingComponent';
 
 function RenderStall({ blog }) {
   return (
