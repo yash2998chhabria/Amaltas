73199b8044ddbab13ff469cd8a62fb8a565c3428
Yash Chhabria <yash2998chhabria@gmail.com>
2020-08-29 21:18:20 +0530

    testimonials model

--- home/models.py
+++ home/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from PIL import Image
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 # Create your models here.
 class PageCounter( models.Model ):
@@ -6,3 +8,26 @@ class PageCounter( models.Model ):
 
 class MakeVisible(models.Model):
 	start_exhibition = models.BooleanField(default=False,null=False)
+
+class Testimonials(models.Model):
+	name= models.CharField(max_length=30,default="",null=False)
+	testi = models.CharField(max_length=300,default="",null=False)	
+	testi_image = models.ImageField(upload_to='testimonials',default="",null=False)
+
+	def save(self, *args, **kwargs):
+		if not self.id:
+			self.testi_image = self.compress(self.testi_image)
+		super(Testimonials, self).save(*args, **kwargs)
+
+	def compress(self,image):
+		im = Image.open(image)
+		im = im.convert('RGB')
+		im_io = BytesIO() 
+		im.save(im_io, 'JPEG', quality=60)
+		im_io.seek(0)
+		new_image = InMemoryUploadedFile(im_io,'ImageField', "%s.jpg" % image.name.split('.')[0], 'image/jpeg', sys.getsizeof(im_io), None)
+		return new_image		
+
+
+	def __str__(self):
+			return self.name
