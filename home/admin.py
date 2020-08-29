4ea70f0bc9a9360401d484517e26f468ca8de5da
Yash Chhabria <yash2998chhabria@gmail.com>
2020-08-29 21:19:57 +0530

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import PageCounter,MakeVisible
+from .models import PageCounter,MakeVisible,Testimonials
 # Register your models here.
 admin.site.register(MakeVisible)
 admin.site.register(PageCounter)
+admin.site.register(Testimonials)
