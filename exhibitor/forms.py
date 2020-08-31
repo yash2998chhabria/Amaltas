b8731c12b5be164a1040268ca81f34c3e9154310
Yash Chhabria <yash2998chhabria@gmail.com>
2020-08-31 17:46:45 +0530

    Update forms.py

--- exhibitor/forms.py
+++ exhibitor/forms.py
@@ -27,7 +27,9 @@ class EditStallFrameForm(forms.ModelForm):
 				'cover':"please upload only a 1:1 sqaure aspect ratio image(if not, it will get cropped)",
 				'description':"please restrict the description to around 30 words or less",
 				'stall_visible_on_website': "This will work only during an exhibition",
-				'contact_stall':"Additional Contact Details"
+				'contact_stall':"Additional Contact Details",
+				'second_category':"If you dont want to choose a second category, leave this field as is",
+				'third_category':"If you dont want to choose a third category, leave this field as is"
 		}
 
 class EditStallProductsForm(forms.ModelForm):
